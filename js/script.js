3d212454a69673981ee7258bd4691f26e6bfeb17
Rom-Freedom <romeo.dark@mail.ru>
2024-09-28 14:05:50 +0300

    add stop__scroll class

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const nav = document.getElementById('nav');
 burger.addEventListener('click', function() {
     nav.classList.toggle('nav-active')
     burger.classList.toggle('burger-active')
+    document.body.classList.toggle('stop__scroll')
 });
 
 
