60ce5860808de2b7d9ada3314113aa8bd7574387
Rom-Freedom <romeo.dark@mail.ru>
2024-09-28 14:01:37 +0300

    activate the burger menu

--- js/script.js
+++ js/script.js
@@ -1,80 +1,10 @@
-const menuBtn = document.getElementById('menu-btn');
-const navLinks = document.getElementById('nav-links');
-const menuBtnIcon = menuBtn.querySelector('i');
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav');
 
-menuBtn.addEventListener('click', (e) => {
-    navLinks.classList.toggle('open');
-
-    const isOpen = navLinks.classList.contains('open');
-    menuBtnIcon.setAttribute('class', isOpen ? 'ri-close-line' : 'ri-menu-line')
+burger.addEventListener('click', function() {
+    nav.classList.toggle('nav-active')
+    burger.classList.toggle('burger-active')
 });
 
-navLinks.addEventListener('click', (e) => {
-    navLinks.classList.remove('open');
-    menuBtnIcon.setAttribute('class', 'ri-menu-line');
-});
 
-const scrollRevealOption = {
-    origin: 'bottom',
-    distance: '50px',
-    duration: 1000,
-};
 
-ScrollReveal().reveal('.header__image img', {
-    ...scrollRevealOption,
-    origin: 'right',
-});
-ScrollReveal().reveal('.header__content p', {
-    ...scrollRevealOption,
-    delay: 500,
-});
-ScrollReveal().reveal('.header__content h1', {
-    ...scrollRevealOption,
-    delay: 1000,
-});
-ScrollReveal().reveal('.header__btns', {
-    ...scrollRevealOption,
-    delay: 1500,
-});
-
-/* DESTINATION */
-ScrollReveal().reveal('.destination__card', {
-    ...scrollRevealOption,
-    interval: 500,
-});
-
-/* SHOWCASE */
-ScrollReveal().reveal('.showcase__image img', {
-    ...scrollRevealOption,
-    origin: 'right',
-});
-ScrollReveal().reveal('.showcase__content h4', {
-    ...scrollRevealOption,
-    delay: 500,
-});
-ScrollReveal().reveal('.showcase__content p', {
-    ...scrollRevealOption,
-    delay: 1000,
-});
-ScrollReveal().reveal('.showcase__btn', {
-    ...scrollRevealOption,
-    delay: 1500,
-});
-
-/* BANNER */
-ScrollReveal().reveal('.banner__card', {
-    ...scrollRevealOption,
-    interval: 500,
-});
-
-/* DISCOVER */
-ScrollReveal().reveal('.discover__card', {
-    ...scrollRevealOption,
-    interval: 500,
-});
-
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    loop: true,
-});
