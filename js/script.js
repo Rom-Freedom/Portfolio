586afd1e21f6acb7aef4687ed09e75993f0e2035
Rom-Freedom <romeo.dark@mail.ru>
2024-09-29 12:37:39 +0300

    activate the slider

--- js/script.js
+++ js/script.js
@@ -32,5 +32,17 @@ window.addEventListener('keydown', function(event) {
     }
 })
 
+//Gallery
+const swiper = new Swiper('#gallery', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+
+    navigation: {
+        prevEl: '#gallery-prev',
+        nextEl: '#gallery-next'
+    }
+});
+
 
 
