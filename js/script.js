1a88597ffa1c4e03281916c681c0d238863d3219
Rom-Freedom <romeo.dark@mail.ru>
2024-09-29 11:50:41 +0300

    activate the modal window

--- js/script.js
+++ js/script.js
@@ -7,5 +7,30 @@ burger.addEventListener('click', function() {
     document.body.classList.toggle('stop__scroll')
 });
 
+// Modal window
+const callFormBtn = document.getElementById('call-form');
+const modalCallForm = document.getElementById('modal-call-form');
+
+//Open the moadal window 
+callFormBtn.addEventListener('click', function() {
+    modalCallForm.classList.add('modal-parent--open')
+})
+
+// Close the modal window
+modalCallForm.querySelector('.modal').addEventListener('click', function(event) {
+    event._isClick = true
+})
+modalCallForm.addEventListener('click', function(event) {
+    if (event._isClick === true) return
+    modalCallForm.classList.remove('modal-parent--open')
+})
+
+//Close when pressing Esc
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        modalCallForm.classList.remove('modal-parent--open')
+    }
+})
+
 
 
